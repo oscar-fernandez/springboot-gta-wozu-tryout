1b31e5115ebb4ecdb716c064f20c7bf8b5dd611a
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2022-08-09 14:20:27 -0400

    Push test

--- src/main/java/edu/cpcc/labs/SpringbootGtaWozuTryoutApplication.java
+++ src/main/java/edu/cpcc/labs/SpringbootGtaWozuTryoutApplication.java
@@ -9,5 +9,5 @@ public class SpringbootGtaWozuTryoutApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGtaWozuTryoutApplication.class, args);
 	}
-
+//test
 }
